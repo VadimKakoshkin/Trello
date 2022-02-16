25ec314c1fc33aaa6a0c0e9bd8c3031e70469b44
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-02-16 12:20:25 +0300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import timer from './modules/timer';
 import scrollUp from './modules/scrollUp';
 import modalHeader from './modules/modalHeader';
 import modalServices from './modules/modalServices';
+
+timer('23 february 2022');
+scrollUp();
+modalHeader();
+modalServices();
