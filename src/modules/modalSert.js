30f919751aa98f179fc43edb6c3f7869ba6cd46a
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-02-25 13:57:36 +0300

    Updated more modules

--- src/modules/modalSert.js
+++ src/modules/modalSert.js
@@ -1,20 +1,52 @@
 //Модальное окно сертификата
 const modalSert = () => {
-    const sertificateImages = document.querySelectorAll('.sertificate-document');
-    const overlay = document.querySelector('.overlay');
-    const hideImages = document.querySelector('.hide_sertificate');
-    const closeServiceModal = document.querySelector('.sertificate-modal__close');
+    const btns = document.querySelectorAll(".sertificate-document");
+    const overlay = document.querySelector(".overlay");
 
-    sertificateImages.forEach(sertificateImage => sertificateImage.addEventListener('click', (e) => {
-        e.preventDefault();
-        hideImages.style.display = 'block';
-        overlay.style.display = 'block';
-        
-    }))
-    closeServiceModal.addEventListener('click', () => {
-        hideImages.style.display = 'none';
-        overlay.style.display = 'none';
-    })
-}
+    const modalImg = document.createElement('div');
+    modalImg.classList.add('header-modal', 'header-modal--opened');
+    modalImg.style.cssText='max-width: 42%;'
+    document.body.append(modalImg);
 
+    const btnClose = document.querySelector('.header-modal__close')
+    const btnImg = btnClose.cloneNode(true);
+    btnImg.style.cssText='background: #4f686e; padding: 8px; border-radius: 50%; color: #fff;'
+    modalImg.append(btnImg);
+
+    const docResource = document.querySelector('.sertificate-document .img-responsive')
+    const docImg = docResource.cloneNode(true);
+    docImg.src = "images/documents/original/document4.jpg";
+    modalImg.append(docImg);
+
+    //Условие
+    btns.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            modalImg.style.display = 'flex';
+            overlay.style.display = "block";
+        });
+    });
+  
+    //Клик
+    btnImg.addEventListener('mouseenter', function (e) { 
+        e.target.style.transition = '1s';
+        e.target.style.background = '#5d5d5d';
+    });
+    
+    btnImg.addEventListener('mouseleave', function (e) {
+        e.target.style.transition = '1s';
+        e.target.style.background = '#4f686e'; 
+    });
+
+    btnImg.addEventListener("click", () => {
+        modalImg.style.display = 'none';
+        overlay.style.display = "none";
+    });
+
+    overlay.addEventListener("click", () => {
+        modalImg.style.display = 'none';
+        overlay.style.display = "none";
+    });
+};
+  
 export default modalSert;
